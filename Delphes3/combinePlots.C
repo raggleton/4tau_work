bc2b249512d307f8fefc22afc3784cd88f3fde1e
Robin Aggleton <robinaggleton@gmail.com>
2014-03-31 22:55:29 +0100

    Update combinePlots to include mu swap case

--- Delphes3/combinePlots.C
+++ Delphes3/combinePlots.C
@@ -16,7 +16,6 @@ void combinePlots(){
 	leg.AddEntry(hSig,"Signal","l");
 	leg.AddEntry(hBg,"Bg","l");
 	leg.Draw();
-	leg.Draw();
 	c1.SaveAs("combined_NTrackCum1.pdf");
 
 	TH1D* hSig2 = fSig.Get("hNTracksCum2");
@@ -30,4 +29,24 @@ void combinePlots(){
 	fSig.Close();
 	fBg.Close();
 
+	TFile fSigRand("Signal_1prong_500K_bare/output_bare_sig_muRand.root","READ");
+	TFile fBgRand("QCDb_mu_pthatmin20_bare/output_bare_bg_muRand.root","READ");
+	TH1D* hSigRand = fSigRand.Get("hNTracksCum1");
+	TH1D* hBgRand = fBgRand.Get("hNTracksCum1");
+	hSigRand->Draw("HISTE");
+	hBgRand->SetLineColor(kRed);
+	hBgRand->Draw("HISTESAME");
+	leg.Draw();
+	c1.SaveAs("combined_NTrackCum1_muRand.pdf");
+
+	TH1D* hSigRand2 = fSigRand.Get("hNTracksCum2");
+	TH1D* hBgRand2 = fBgRand.Get("hNTracksCum2");
+	hSigRand2->Draw("HISTE");
+	hBgRand2->SetLineColor(kRed);
+	hBgRand2->Draw("HISTESAME");
+	leg.Draw();
+	c1.SaveAs("combined_NTrackCum2_muRand.pdf");
+
+	fSigRand.Close();
+	fBgRand.Close();
 }
