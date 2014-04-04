ddf0746647aeebe1a7231fe39699edb00c8ae63f
Robin Aggleton <robinaggleton@gmail.com>
2014-04-04 10:18:02 +0100

    Update Delphes PBS script

--- Delphes3/submitDelphesLots.sh
+++ Delphes3/submitDelphesLots.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
-for name in ${HOME}/pythia8183/examples/QCDb_*.hepmc
+# for name in ${HOME}/pythia8183/examples/qcdb_*.hepmc
+# for name in ${HOME}/pythia8183/QCDb_mu_pthatmin20/qcdb_pthatmin20_*.hepmc
+for name in ${HOME}/pythia8183/QCDb_mu_pthatmin20_Mu17_Mu8/qcdb_pthatmin20_Mu17_Mu8_*.hepmc
 do
-	filename=${name#*examples/}
-	echo "Doing "$filename
-	num=${filename#QCDb_}
-	out="QCDb_mu_$num"	
-	qsub -v hepmcname=${name},rootname="/panfs/panasas01/phys/ra12451/Delphes-3.0.12/examples/QCDb_mu_cleanTk/"${out%hepmc}root runDelphesBatch.sh
-	sleep 10
+	filename=${name##*/}
+	num=${filename##*_}
+	pureNum=${num%.hepmc}
+	# echo $pureNum
+	if [ "$pureNum" -gt "120" ]
+	then
+		echo "Doing "$filename
+		out="QCDb_mu_pthatmin20_Mu17_Mu8_$pureNum"	
+		# echo $out
+		# echo $name
+		qsub -v hepmcname=${name},rootname="/panfs/panasas01/phys/ra12451/Delphes-3.0.12/QCDb_mu_pthatmin20_Mu17_Mu8_bare/"${out}.root runDelphesBatch.sh
+		sleep 10
+	fi
 done
