393bb4923a5eea11a9057da00dc8dae21f372d38
Robin Aggleton <robinaggleton@gmail.com>
2014-04-07 11:31:28 +0100

    Improve output for user clarity

--- Delphes3/mainAnalysis.C
+++ Delphes3/mainAnalysis.C
@@ -16,16 +16,20 @@ void mainAnalysis()
 
 	gSystem->Load("libDelphes");
 
-	bool doSignal = false;
+	bool doSignal = true;
 	bool doMu = true; // for QCDb - either inclusive decays or mu only decays
 	bool swapMuRandomly = false; // if true, fills plots for mu 1 and 2 randomly from highest & 2nd highest pt muons. Otherwise, does 1 = leading (highest pt), 2 = subleading (2nd highest pt)
-	bool doHLT = true; // for signal MC - require HLT conditions or not
+	bool doHLT = true; // for signal MC - require HLT conditions or not. TODO: QCDb
 
 	// Create chain of root trees
 	TChain chain("Delphes");
 	addInputFiles(&chain, doSignal, doMu, doHLT);
 
 	if (swapMuRandomly) cout << "Swapping mu 1<->2 randomly" << endl;
+	else cout << "mu1 has higher pT than mu2" << endl;
+	
+	if (doHLT) cout << "Using files with HLT cuts already made" << endl;
+	else cout << "Using file swihtout any HLT cut" << endl;
 
 	// Create object of class ExRootTreeReader
 	ExRootTreeReader *treeReader = new ExRootTreeReader(&chain);
@@ -550,7 +554,9 @@ void mainAnalysis()
 	if (swapMuRandomly)
 		app += "_muRand";
 	
-	if (!doHLT)
+	if (doHLT)
+		app += "_HLT";
+	else
 		app += "_NoHLT";
 
 	// Get directory that input file was in - put plots in there
