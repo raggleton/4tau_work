837759ff9f407a7d2d60814d587e7ddc70e53dae
Robin Aggleton <robinaggleton@gmail.com>
2014-05-29 14:04:46 +0100

    Clean up formatting, make line < 80 characters in length (well, nearly all)

--- Delphes3/commonFunctions.h
+++ Delphes3/commonFunctions.h
@@ -26,7 +26,9 @@ namespace fs = boost::filesystem;
 namespace po = boost::program_options;
 
 /**
- * This header contains common functions for all my Delphes analysis scripts
+ * This header contains common functions for all my Delphes analysis scripts.
+ *
+ * For classes/functions pertaining to user options, see programOpts.h
  *
  * Robin Aggleton 2014
  */
@@ -36,9 +38,11 @@ namespace po = boost::program_options;
  * @param s     string to be split
  * @param delim character to be used as delimiter, e.g. ':'
  * @param elems blank vector to be used for split results
- * @return      Returns the input vector elems, filled with s split based on delim
+ * @return      Returns input vec <elems>, filled with s split based on delim
  */
-std::vector<std::string> &split(const std::string &s, char delim, std::vector<std::string> &elems) {
+std::vector<std::string> &split(const std::string &s, 
+								char delim, 
+								std::vector<std::string> &elems) {
 	std::stringstream ss(s);
 	std::string item;
 	while (std::getline(ss, item, delim)) {
@@ -48,7 +52,7 @@ std::vector<std::string> &split(const std::string &s, char delim, std::vector<st
 }
 
 // Get directory that input file was in - put plots in there
-std::string getDirectory(TFile* f){	
+std::string getDirectory(TFile* f) {	
 	std::string fullpath = f->GetDirectory("")->GetName();
 	std::vector<std::string> elems;
 	split(fullpath, '/', elems);
@@ -56,7 +60,7 @@ std::string getDirectory(TFile* f){
 }
 
 // Get Delphes file config used - last part of directory name
-std::string getDelph(std::string directory){	
+std::string getDelph(std::string directory) {	
 	std::vector<std::string> elems2;
 	split(directory, '_', elems2);
 	return elems2[elems2.size()-1];
@@ -65,13 +69,15 @@ std::string getDelph(std::string directory){
 /**
  * Dermine number of events to run over, based on user input.
  * If user inputs "-1", it will return all events in tree.
- * Note that if the user doesn't specify anything, by default it returns all events in tree.
+ * Note that if the user doesn't specify anything, 
+ * by default it returns the total number of events in tree.
  * @param  treeReader The Delphes tree, used to determine total # events
  * @param  pOpts      ProgramOpts object that contains user's options
- * @return            Returns either total numbers of events in the tree, or the number of events the user specified.
+ * @return            Returns either total numbers of events in the tree, 
+ *                    or the number of events the user specified.
  */
-Long64_t getNumberEvents(ExRootTreeReader *treeReader, ProgramOpts* pOpts){
-	if (pOpts->getNEvents() == -1){
+Long64_t getNumberEvents(ExRootTreeReader *treeReader, ProgramOpts* pOpts) {
+	if (pOpts->getNEvents() == -1) {
 		return treeReader->GetEntries();
 	} else {
 		return pOpts->getNEvents();
@@ -85,7 +91,7 @@ Long64_t getNumberEvents(ExRootTreeReader *treeReader, ProgramOpts* pOpts){
  * @return   Returns TRUE if pT of obj a > pT obj b, FALSE otherwise
  */
 template <typename T>
-bool sortByPT(T* a, T* b){
+bool sortByPT(T* a, T* b) {
 	if(a->PT > b->PT) {
 		return true; 
 	} else {
@@ -94,14 +100,19 @@ bool sortByPT(T* a, T* b){
 }
 
 /**
- * From the daughters of 2 taus from a given "a" Higgs, decide which is track and which is mu
+ * From the daughters of 2 taus from a given "a" Higgs, 
+ * decide which is track and which is mu
  * @param  mu muon GenParticle, will be assigned to an object (a or b)
  * @param  tk track GenParticle, will be assigned to an object (a or b)
  * @param  a  One of the two gen particles from a pair of taus
  * @param  b  The other of the two GenParticles from th pair of taus
- * @return    Returns TRUE if assignemnt OK - if neither a nor b is a muon then returns FALSE
+ * @return    Returns TRUE if assignemnt OK - 
+ *            if neither a nor b is a muon then returns FALSE
  */
-bool assignMuonAndTrack(GenParticle* &mu, GenParticle* &tk, GenParticle &a, GenParticle &b){
+bool assignMuonAndTrack(GenParticle* &mu, 
+						GenParticle* &tk, 
+						GenParticle &a, 
+						GenParticle &b) {
 	tk = 0;
 	mu = 0;
 
@@ -122,7 +133,7 @@ bool assignMuonAndTrack(GenParticle* &mu, GenParticle* &tk, GenParticle &a, GenP
 	}
 
 	// the muon is the mu with the higest pT, the other mu becomes a track
-	if (aIsMu && bIsMu){
+	if (aIsMu && bIsMu) {
 		if (a.PT > b.PT) {
 			mu = &a;
 			tk = &b;
@@ -143,168 +154,112 @@ bool assignMuonAndTrack(GenParticle* &mu, GenParticle* &tk, GenParticle &a, GenP
  * @param branchAll  Branch of ALL GenParticles
  * @param cand       Particle whose daughters you wish to print to screen 
  */
-void printParticleDaughters(TClonesArray *branchAll, GenParticle* cand){
-	if (cand->D1 != -1 && cand->D2 != -1){
-		for(int i = cand->D1; i <= cand->D2; i++){
-			cout << "Daughter at " << i << ", PID: " << ((GenParticle*)branchAll->At(i))->PID << endl;
+void printParticleDaughters(TClonesArray *branchAll, GenParticle* cand) {
+	if (cand->D1 != -1 && cand->D2 != -1) {
+		for(int i = cand->D1; i <= cand->D2; i++) {
+			cout << "Daughter at " << i << ", PID: " << 
+			((GenParticle*)branchAll->At(i))->PID << endl;
 		}
 	} else
 		cout << "No daughters, stable." << endl;
 }
 
 /**
- * Get the 3 immediate daughters of the tau (tau neutrino + qqbar or lepton+neutrino)
- * @param branchAll  The branch of ALL gen particles
- * @param tau        Tau to get decay product from
- * @return           Vector of the tau's 3 immediate daughters
- */
-
-// CURRENTLY DEPRECIATED
-
-// std::vector<GenParticle*> getTauDaughters(TClonesArray *branchAll, GenParticle *tau) { 
-
-// 	cout << "get tau daughters" << endl;
-
-// 	std::vector<GenParticle*> tauDaughters;
-
-// 	bool foundProduct = false;
-// 	while(!foundProduct){
-// 		cout << tauDaughters.size() << endl;
-// 		cout << "D1: " << tau->D1 << endl;
-// 		cout << "D2: " << tau->D2 << endl;
-		
-// 		printParticleDaughters(branchAll, tau);
-
-// 		if(tau->D1 == tau->D2) { // tau -> tau
-// 			cout << "D1 = D2" << endl;
-// 			tau = (GenParticle*) branchAll->At(tau->D1);
-// 		} else if ((tau->D2)-(tau->D1) == 1) { //tau->tau+gamma
-			
-// 			cout << "Tau -> tau + gamma" <<endl;
-			
-// 			if ( fabs(((GenParticle*) branchAll->At(tau->D1))->PID) == 22) {
-			
-// 				cout << "Tau is " << tau->D2 << " check " << ((GenParticle*) branchAll->At(tau->D2))->PID << endl;
-// 				tau = (GenParticle*) branchAll->At(tau->D2);
-			
-// 			} else{
-			
-// 				cout << "Tau is " << tau->D1 << " check " << ((GenParticle*) branchAll->At(tau->D1))->PID << endl;
-// 				tau = (GenParticle*) branchAll->At(tau->D1);
-// 			}
-		
-// 		} else {
-// 			for(int i = tau->D1; i <= tau->D2; i++){
-// 				tauDaughters.push_back((GenParticle*) branchAll->At(i));
-// 				// tauDaughters.push_back(i);
-// 			}
-// 			if (tauDaughters.size() ==3){
-// 				cout << "3 products" << endl;
-// 				foundProduct = true;
-// 				return tauDaughters;
-// 			}
-// 		}
-// 	}
-// }
-
-/**
  * From tau, get the final, stable, charged decay product.
- * Does this by stepping through "generations" - looking at daughters of all the particles in "current" vector
+ * Does this by stepping through "generations" 
+ * - looking at daughters of all the particles in "curr" vector
  * Eliminates any duplicates, (either in previoius or current generations),
  * and if the particle is final state, we return it.
  * Checks at the end to ensure only 1 charged final state particle.
  * 
- * @param  branchAll The branch of ALL gen particles, to loop through decay chain
+ * @param  branchAll The branch of ALL gen particles, to go through decay chain
  * @param  tau       Tau to get decay product from
  * @return           Returns charged, stable, decay product of param tau
  */
 GenParticle* getChargedObject(TClonesArray* branchAll, GenParticle* tau) { 
-
-	std::vector<GenParticle*> history; // hold all *unique* particles in the decay chain (stores position number of particle in event listing)
-	std::vector<GenParticle*> current = { tau };
-	std::vector<GenParticle*> next; // holds decay products, not nec. all unique
+	
+	// hold all *unique* particles in the decay chain 
+	// (stores position number of particle in event listing)
+	std::vector<GenParticle*> history; 
+	
+	std::vector<GenParticle*> curr = { tau };
+	
+	// holds decay products from particles in curr, not necessarily all unique
+	std::vector<GenParticle*> next; 
 	
 	GenParticle *prong = nullptr; // holds final charged product
 	
 	int nCharged = 0; // count charged particles - shouldn't have more than 1!
 	bool foundOne = false;
 
-	while (current.size()>0){ // if current > 0 we haven't gone through all the particles
-		for (unsigned currentElem1 = 0; currentElem1 < current.size(); currentElem1++){
+	while (curr.size()>0) { // if 0 we haven't gone through all the particles
+		for (unsigned currElem1 = 0; currElem1 < curr.size(); currElem1++) {
 			
-			// Check 1 - is this already in current?
-			// Could probably do more efficiently using the unique function on std::vector
+			// Check 1 - is this already in curr?
+			// Could probably do more efficiently using the unique function
 			bool alreadyDone = false;
 
-			for (unsigned currentElem2 = 0; currentElem2 < currentElem1; currentElem2++){
-				if ((current[currentElem1] == current[currentElem2]) && (currentElem1 != 0)) {
-					// cout << "--Found a duplicate" << endl;
+			for (unsigned currElem2 = 0; currElem2 < currElem1; currElem2++) {
+				if ((curr[currElem1] == curr[currElem2]) && (currElem1 != 0)) {
 					alreadyDone = true;
 				}
 			}
 
 			// Check 2 - is this already in history?
-			if (!alreadyDone){
-				// cout << "-not in current" << endl;
-				for (unsigned historyElem = 0; historyElem < history.size(); historyElem++){
-					if ((current[currentElem1] == history[historyElem]) && (historyElem!=0)){
-						// cout << "--Found a dupicate in history" << endl;
+			if (!alreadyDone) {
+				for (unsigned historyElem = 0; historyElem < history.size(); historyElem++) {
+					if ((curr[currElem1] == history[historyElem]) && (historyElem!=0)) {
 						alreadyDone = true;
 					}
 				}
 			}
 
 			// Check 3 - is this final state?
-			// cout << alreadyDone << endl;
-			if (!alreadyDone){
-				// cout << "-not in history" << endl;
-
+			if (!alreadyDone) {
 				// Check if final state. Either status 1, or D1 == D2 == -1
-				if (current[currentElem1]->Status == 1 || (current[currentElem1]->D1 == -1 && current[currentElem1]->D2 == -1) ) {
+				if (curr[currElem1]->Status == 1 || 
+					(curr[currElem1]->D1 == -1 && curr[currElem1]->D2 == -1)) {
 
 					// Check if charged
-					if (current[currentElem1]->Charge != 0) {
-						// cout << "FINAL PRONG " << current[currentElem1]->PID << endl;
-						prong = current[currentElem1];
+					if (curr[currElem1]->Charge != 0) {
+						prong = curr[currElem1];
 						foundOne = true;
 						nCharged++;
 					}
 
-					history.push_back(current[currentElem1]);
-					// cout << "pushed into history" << endl;
+					history.push_back(curr[currElem1]);
 				} else {
 					// Load its daughters no. into next
-					for (int daughter = current[currentElem1]->D1; daughter <= current[currentElem1]->D2; daughter++) {
-						// cout << "pushing" << endl;
+					for (int daughter = curr[currElem1]->D1; 
+						 daughter <= curr[currElem1]->D2; daughter++) {
 						next.push_back((GenParticle*) branchAll->At(daughter));
 					}
 
 					// Load it into history
-					history.push_back(current[currentElem1]);
+					history.push_back(curr[currElem1]);
 				}
 
 			}// end of alreadyDone
-		} // end of loop over current
+		} // end of loop over curr
 
-		// Clear current - don't need to do as = assignment auto does this
-		// current.clear();
-		// Copy next into current
-		current = next;
+		// Clear curr - don't need to do as = assignment auto does this
+		// curr.clear();
+		// Copy next into curr
+		curr = next;
 		// Empty next
 		next.clear();
 	} // end of while(!done)
 
 	// Just in case
 	history.clear();
-	current.clear();
+	curr.clear();
 	next.clear();
 
 	if (!foundOne) {
 		cout << "No prongs!!!" << endl;
 	}
 
-	if (nCharged == 1){
-		// cout << "PRONG: " << prong->PID << endl;
+	if (nCharged == 1) {
 		return prong;
 	} else {
 		cout << "Damn, more than 1 prong!!!! Got " << nCharged << endl;
@@ -314,15 +269,24 @@ GenParticle* getChargedObject(TClonesArray* branchAll, GenParticle* tau) {
 }
 
 /**
- * Draw a histogram (1D or 2D!) and save it to PDF, as <directory>/<filename>_<delphes setup from <directory>>_<app>.pdf
- * @param h         Hist to draw. Since THStack and TH1 don't share SetMarkerSize method, need to check object type. Suppose I should use specialisation here instead...
+ * Draw a histogram (1D or 2D!) and save it to PDF, 
+ * as <directory>/<filename>_<delphes setup from <directory>>_<app>.pdf
+ * @param h         Hist to draw. Since THStack and TH1 don't share 
+ *                  SetMarkerSize method, need to check object type. 
+ *                  Suppose I should use specialisation here instead...
  * @param drawOpt   Options for drawing hist
  * @param filename  Main filename (e.g. Mu1Pt)
  * @param directory Directory for output PDF
  * @param app       Appendage eg muRand, sig
  */
 template<typename T>
-void drawHistAndSave(T* h, std::string drawOpt, std::string filename, std::string directory, std::string app, bool drawLogY = false){
+void drawHistAndSave(T* h, 
+					 std::string drawOpt, 
+					 std::string filename, 
+					 std::string directory, 
+					 std::string app, 
+					 bool drawLogY = false) {
+
 	gStyle->SetOptStat("ne"); // display name and # entries only
 	gStyle->SetPaintTextFormat(".3g"); // set text format to be printed
 	
@@ -332,33 +296,36 @@ void drawHistAndSave(T* h, std::string drawOpt, std::string filename, std::strin
 	if (drawLogY) c.SetLogy();
 	
 	// For plots drawing text values of bins, make text bigger
-	if (!std::is_same<T, THStack>::value){
-		if (drawOpt.find("TEXT") != std::string::npos){
+	if (!std::is_same<T, THStack>::value) {
+		if (drawOpt.find("TEXT") != std::string::npos) {
 			h->SetMarkerSize(1.5*h->GetMarkerSize());
 		}
 	}
 
 	h->Draw(drawOpt.c_str());
 
-	// To get the delphes config used, which is always the last part of the directory name
+	// To get the delphes config used, 
+	// which is always the last part of the directory name
 	std::string delph = getDelph(directory);
+	
 	// Save to PDF
 	c.SaveAs((directory+"/"+filename+"_"+delph+"_"+app+".pdf").c_str());
 }
 
 /**
  * Rescale histogram so integral = unity
- * @param h Poitner to histogram to be rescaled
+ * @param h Pointer to histogram to be rescaled
  */
-void normaliseHist(TH1* h){
-	if (h->Integral() != 0){
+void normaliseHist(TH1* h) {
+	if (h->Integral() != 0) {
 		h->Scale(1./h->Integral());
 	}
 }
 
 
 /**
- * Draws mass correlation plot, saves file as directory/filename_<delphes setup from directory>_app.pdf
+ * Draws mass correlation plot, saves file as 
+ * directory/filename_<delphes setup from directory>_app.pdf
  * @param title         Title at top of plot
  * @param histM1_0to1   Pointer to TH1 for m2 bin 0 to 1
  * @param histM1_1to2   Pointer to TH1 for m2 bin 1 to 2
@@ -368,7 +335,14 @@ void normaliseHist(TH1* h){
  * @param directory     Directory to put output file in
  * @param app           Any appendage onto the filename e.g. muRand
  */
-void drawMassPlot(std::string title, TH1* histM1_0to1, TH1* histM1_1to2, TH1* histM1_2to3, TH1* histM1_3toInf, std::string filename, std::string directory, std::string app){
+void drawMassPlot(std::string title, 
+				  TH1* histM1_0to1, 
+				  TH1* histM1_1to2, 
+				  TH1* histM1_2to3, 
+				  TH1* histM1_3toInf, 
+				  std::string filename, 
+				  std::string directory, 
+				  std::string app) {
 
 	gStyle->SetOptStat("");
 	gStyle->SetLegendBorderSize(0);
@@ -380,7 +354,8 @@ void drawMassPlot(std::string title, TH1* histM1_0to1, TH1* histM1_1to2, TH1* hi
 	c.SetCanvasSize(500, 600);
 	TPad* pad1 = new TPad("pad1","",0,0.30,1,1);
 	pad1->SetBottomMargin(0.02);
-	pad1->SetRightMargin(0.05); // The ratio plot below inherits the right and left margins settings here!
+	// The ratio plot below inherits the right and left margins settings here!
+	pad1->SetRightMargin(0.05);
 	pad1->SetLeftMargin(0.15);
 	pad1->Draw();
 	pad1->cd();
@@ -433,7 +408,7 @@ void drawMassPlot(std::string title, TH1* histM1_0to1, TH1* histM1_1to2, TH1* hi
 	pad2->SetLeftMargin(pad1->GetLeftMargin());
 	pad2->SetRightMargin(pad1->GetRightMargin());
 	pad2->SetTopMargin(0.05);
-	pad2->SetTicks(1,1); // Puts tick marks on upper x axis and right y axis (yes, fn of Pad, not the Hist...)
+	pad2->SetTicks(1,1); // Put tick marks on upper x and right y axes (fn of Pad, not the Hist!)
 	pad2->Draw();
 	pad2->cd();
 
@@ -471,7 +446,8 @@ void drawMassPlot(std::string title, TH1* histM1_0to1, TH1* histM1_1to2, TH1* hi
 	line->SetLineStyle(2);
 	line->Draw();
 
-	// To get the delphes config used, which is always the last part of the directory name
+	// To get the delphes config used, 
+	// which is always the last part of the directory name
 	std::string delph = getDelph(directory);
 	
 	// Save to PDF
