420d2547bf82658b7b8ee540c8e0a872110e4cbd
Robin Aggleton <robinaggleton@gmail.com>
2014-06-02 10:25:17 +0100

    Add commands for QCDScatter

--- Delphes3/Scripts/runDelphesLots.sh
+++ Delphes3/Scripts/runDelphesLots.sh
@@ -5,7 +5,8 @@
 # for name in ${HOME}/pythia8183/examples/qcdb_*.hepmc
 # for name in ${HOME}/pythia8183/QCDb_mu_pthatmin20/qcdb_pthatmin20_*.hepmc
 # for name in ${HOME}/pythia8183/QCDb_mu_pthatmin20_Mu17_Mu8/qcdb_pthatmin20_Mu17_Mu8_*.hepmc
-for name in ${HOME}/pythia8183/QCDc_mu_pthatmin20_Mu17_Mu8/qcdc_pthatmin20_*.hepmc
+# for name in ${HOME}/pythia8183/QCDc_mu_pthatmin20_Mu17_Mu8/qcdc_pthatmin20_*.hepmc
+for name in ${HOME}/pythia8183/QCDScatter_mu_pthatmin20_Mu17_Mu8/qcdScatter_pthatmin20_*.hepmc
 # for name in ${HOME}/pythia8183/QCDb_mu_pthatmin20/qcdb_pthatmin20_*.hepmc
 do
 	filename=${name##*/}
@@ -14,17 +15,20 @@ do
 	# stem=${filename%_NoHLT.hepmc}
 	stem=${filename%_HLT.hepmc}
 	# pureNum=${stem#qcdb_pthatmin20_Mu17_Mu8_}
-	pureNum=${stem#qcdc_pthatmin20_}
-	if [ "$pureNum" -gt "100" ]
-	then
+	# pureNum=${stem#qcdc_pthatmin20_}
+	pureNum=${stem#qcdScatter_pthatmin20_}
+	#if [ "$pureNum" -gt "220" ]
+	#then
 		echo "Doing "$filename
+		# out="QCDc_mu_pthatmin20_Mu17_Mu8_$pureNum"	
+		out="QCDScatter_mu_pthatmin20_Mu17_Mu8_$pureNum"	
 		echo "Writing to "${out}.root
-		out="QCDc_mu_pthatmin20_Mu17_Mu8_$pureNum"	
 		# echo $out
 		# echo $name
 		# qsub -v hepmcname=${name},rootname="/panfs/panasas01/phys/ra12451/Delphes-3.0.12/QCDb_mu_pthatmin20_Mu17_Mu8_bare/"${out}.root runDelphesBatch.sh
 		# qsub -v hepmcname=${name},rootname="/panfs/panasas01/phys/ra12451/Delphes-3.0.12/QCDc_mu_pthatmin20_Mu17_Mu8_bare/"${out}.root runDelphesBatch.sh
 		# sleep 10
-		../DelphesHepMC ../examples/delphes_card_CMS_bare.tcl /panfs/panasas01/phys/ra12451/Delphes-3.0.12/QCDc_mu_pthatmin20_Mu17_Mu8_bare/${out}.root $name
-	fi
+		# ../DelphesHepMC ../examples/delphes_card_CMS_bare.tcl /panfs/panasas01/phys/ra12451/Delphes-3.0.12/QCDc_mu_pthatmin20_Mu17_Mu8_bare/${out}.root $name
+		../DelphesHepMC ../examples/delphes_card_CMS_bare.tcl /panfs/panasas01/phys/ra12451/Delphes-3.0.12/QCDScatter_mu_pthatmin20_Mu17_Mu8_bare/${out}.root $name
+	#fi
 done
